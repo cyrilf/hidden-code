1c24808cd778c8319064cf338b797ccbc66b3c80
Cyrilf <cyril.francesconi@gmail.com>
2012-06-01 19:48:13 +0200

    Added comments to code !

--- jquery.hiddenCode.js
+++ jquery.hiddenCode.js
@@ -14,18 +14,23 @@
 		var opts = $.extend({}, $.fn.hiddenCode.defaults, options);
 		return this.each(function() {
 
+			//mastercode is the password send by the user. Else it's 'konami'
 			var masterCode = 'konami';
 			if (typeof opts.password !== 'undefined') {
 			    masterCode = opts.password;
 			}
 
+			//isEncoded == true if the password of the user is an array of key control
+			//if it's a string, it's not encoded so we will do it later
 			var isEncoded = false;
 			if(typeof masterCode !== 'string'){
 				isEncoded = true;
 			}
 
+			//masterKey is the translation of the user password in key control
 			var masterKey = [];
 			if(masterCode!='konami' && !isEncoded){
+				//keyup event doesn't differenciate upper and lowercase, so each password is compare upper
 				masterCode = masterCode.toUpperCase();
 				for(var i = 0; i<masterCode.length;i++){
 					masterKey.push(masterCode.charCodeAt(i));
@@ -36,17 +41,20 @@
 				masterKey = [38,38,40,40,37,39,37,39,66,65];
 			}
 
+			//controllerKey contains the user input as he types
 			var controllerKey = [];
 			var state = false;
 			var valid = false;
 			$(window).keyup(function(evt) {
 				var code = evt.keyCode ? evt.keyCode : evt.which;
 				controllerKey.push(code);
+				//If this letter equals the masterKey one (at the same position)
 				if(controllerKey.length <= masterKey.length && code == masterKey[controllerKey.length-1]){
 					state = true;
 				} else {
 					var firstSame = true;
 					if(controllerKey.length>1){
+						//This code fix a bug (if we have the same character for the first letters we have to keep this characters on memory)(hard to explain but resolve)
 						for(var i=controllerKey.length;i>1;i--){
 							if(controllerKey[i-1] != controllerKey[0]){
 								firstSame = false;
@@ -70,6 +78,7 @@
 				if(state === true && masterKey.length == controllerKey.length){
 					valid = true;
 				}
+				//if the password is found, we execute the callback function and we restore the controllerKey
 				if(valid) {
 					opts.callback();
 					state = valid = false;
